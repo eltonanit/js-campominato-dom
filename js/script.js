0fe28b2094f5e54effb068bf0b93535fb3ffd3c0
eltonanit <elton.ani@gmail.com>
2024-07-15 10:44:23 +0200

    [UPD] Added file index and css  and JS of the previous excercise

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Recupero la griglia
+document.getElementById('generateGrid').addEventListener('click', function () {
+
+    const grid = document.getElementById('grid');
+    grid.innerHTML = '';
+
+    // Genero le celle
+    for (let i = 1; i <= 100; i++) {
+        const square = document.createElement('div');
+        square.className = 'square';
+        square.textContent = i;
+
+        // Aggiungiamo l'evento click alla cella con add event listener
+        square.addEventListener('click', function () {
+
+            // Colora la cella di azzurro
+            this.classList.toggle('active');
+
+            // Mostra il numero della cella in console
+            console.log('Cella cliccata:', i);
+        });
+
+        // Appendo  la cella al contenitore della griglia
+        grid.appendChild(square);
+    }
+});
